92e70d858c19ab0e24b20a29ec1eb9f69a09d8bf
祐真 山田 <yamada@sukezane-no-MacBook-Air.local>
2015-12-31 17:43:00 +0900

    no message

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -40,12 +40,15 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+ config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-:address => 'smtp.gmail.com',
-:port => 587,
-:authentication => :plain,
-:user_name => ‘kommunika1029@gmail.com’
-:password => ‘xray5094’ #password
-}
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "kommunika1029@gmail.com", #ご自身のgmailアドレス
+    :password => "xray5094", #ご自身のgmailアドレスのパスワード
+    :authentication => 'login',
+  }
 end
