8b9e27c258e7edd56563cc9617c4068a30dd6b48
祐真 山田 <yamada@sukezane-no-MacBook-Air.local>
2015-12-31 18:08:49 +0900

    メール送信部

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,10 @@ ActiveRecord::Schema.define(version: 20151231083139) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
+    t.string   "confirmation_token",     limit: 255
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email",      limit: 255
     t.string   "user_id",                limit: 255
     t.integer  "Birth_date",             limit: 4
     t.text     "gender",                 limit: 65535
@@ -38,6 +42,7 @@ ActiveRecord::Schema.define(version: 20151231083139) do
     t.integer  "start_at_band",          limit: 4
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
