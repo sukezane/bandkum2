8e495d39a45fa25da765beee38ebaed3b44d6e36
祐真 山田 <yamada@sukezane-no-MacBook-Air.local>
2016-01-01 14:52:09 +0900

    新規登録項目追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+def datetime_select(object_name, method, options = {}, html_options = {})
+  Tags::DatetimeSelect.new(object_name, method, self, options, html_options).render
+end
+  
 end
