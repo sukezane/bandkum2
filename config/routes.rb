c0fbba76343c314177710e69316d9ee9aaf09255
祐真 山田 <yamada@sukezane-no-MacBook-Air.local>
2016-01-01 18:22:33 +0900

    routesのauthentication,conversationの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  root :to => 'users#index'
+
+  authenticated :user do
+    root :to => 'users#index'
+  end
+
+  unauthenticated :user do
+    devise_scope :user do
+      get "/" => "devise/sessions#new"
+    end
+  end
+  
+  resources :conversations do
+    resources :messages
+  end
+
 end
