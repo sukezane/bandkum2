c4fa5f2135be888420245b2a28d76b9fd3dd2cfb
祐真 山田 <yamada@sukezane-no-MacBook-Air.local>
2016-01-01 16:11:49 +0900

    バリデーションの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << [:name, :part_1,:user_id,:Birth_date,:gender,:start_at_part_1,:start_at_part_2,:current_place,:start_at_band]
+      devise_parameter_sanitizer.for(:sign_in) <<[:name, :part_1,:user_id,:Birth_date,:gender,:start_at_part_1,:start_at_part_2,:current_place,:start_at_band]
+      devise_parameter_sanitizer.for(:account_update) << [:name, :part_1,:user_id,:Birth_date,:gender,:start_at_part_1,:start_at_part_2,:current_place,:start_at_band]
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
          validates :terms, acceptance: true
+         validates :name, presence: true, length: { maximum: 50 }
+ 
 end
